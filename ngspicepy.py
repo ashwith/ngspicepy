76087adac46eb259a4ea8a79e803c349c911b811
Ashwith Jerome Rego <ashwith@gmail.com>
2016-10-08 12:45:59 +0530

    Added the C structs for the shared library.
    Structs added: vecinfoall, vecvalues and vecvaluesall
    Structs Pending: vector_info, vecinfo

--- ngspicepy.py
+++ ngspicepy.py
@@ -1,4 +1,5 @@
-from ctypes import cdll, CFUNCTYPE, c_int, c_bool, c_char_p, c_void_p, create_string_buffer
+from ctypes import cdll, CFUNCTYPE, c_int, c_bool, c_char_p, c_void_p,\
+        c_double, create_string_buffer, Structure
 
 # Load the ngspice shared library.
 # TODO: Figure out the path intelligently
@@ -6,6 +7,27 @@ libngspice = cdll.LoadLibrary("/usr/local/lib/libngspice.so.0")
 
 
 # C structs that are required by the shared library
+
+class vecinfoall(Structure):
+    _fields_ = [("name", c_char_p),
+            ("title", c_char_p),
+            ("date", c_char_p),
+            ("type", c_char_p),
+            ("veccount", c_int),
+            ("vecs", POINTER(POINTER(vecinfo)))]
+
+
+class vecvalues(Structure):
+    _fields_ = [("name", c_char_p),
+            ("creal", c_double),
+            ("cimag", c_double),
+            ("is_scale", c_bool),
+            ("is_complex", c_bool)]
+
+class vecvaluesall(Structure):
+    _fields_ = [("veccount", c_int),
+            ("vecindex", c_int),
+            ("vecsa", POINTER(POINTER(vecvalues))]
 
 
 
