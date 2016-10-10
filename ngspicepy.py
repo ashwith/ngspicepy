5050f5eb337ae3a2f7359e981c4b43b43693b9a9
Ashwith Jerome Rego <ashwith@gmail.com>
2016-10-10 21:53:03 +0530

    Fixed typos and added features
    1. Added queues to store the output generated by SendChar and
    SendStat.
    2. Updated SendChar and SendStat to use these queues.
    3. Added a send_command function to simplify the calling of ngspice
    commands
    4. The shared libary is now initialized on import.

--- ngspicepy.py
+++ ngspicepy.py
@@ -1,10 +1,19 @@
-from ctypes import cdll, CFUNCTYPE, c_int, c_bool, c_char_p, c_void_p,\
-        c_double, create_string_buffer, Structure
+from ctypes import cdll, CFUNCTYPE, c_int, c_bool, c_char_p, c_void_p, c_short,\
+        c_double, create_string_buffer, POINTER, Structure
+
+from queue import Queue, Empty
+import string
+
+# Constants
+TIMEOUT = 0.5
 
 # Load the ngspice shared library.
 # TODO: Figure out the path intelligently
 libngspice = cdll.LoadLibrary("/usr/local/lib/libngspice.so.0") 
 
+send_char_dat = Queue()
+send_stat_dat = Queue()
+is_simulating = False
 
 # C structs that are required by the shared library
 
@@ -17,11 +26,11 @@ class vector_info(Structure):
     _fields_ = [("v_name", c_char_p),
             ("v_type", c_int),
             ("v_flags", c_short),
-            ("v_realdata",c_double_p),
+            ("v_realdata",POINTER(c_double)),
             ("v_compdata",POINTER(ngcomplex_t)),
             ("v_length",c_int)]
 
-class vecinfo(Structure)           
+class vecinfo(Structure):     
   _fields_ = [("number", c_int),
             ("vecname",c_char_p),
             ("is_real", c_bool),
@@ -47,8 +56,7 @@ class vecvalues(Structure):
 class vecvaluesall(Structure):
     _fields_ = [("veccount", c_int),
             ("vecindex", c_int),
-            ("vecsa", POINTER(POINTER(vecvalues))]
-
+            ("vecsa", POINTER(POINTER(vecvalues)))]
 
 
 # Callback functions
@@ -58,23 +66,46 @@ def ControlledExit(exit_status, is_unload, is_quit, lib_id, ret_ptr):
     print(exit_status)
     print(is_unload)
     print(is_quit)
-    print(lib_id)
-    pass
+    return 0
     
 @CFUNCTYPE(c_int, c_char_p, c_int, c_void_p)
 def SendChar(output, lib_id, ret_ptr):
-    print(output)
-    print(lib_id)
+    global send_char_dat
+
+    clean_output = "".join(output.decode().split('*'))
+    #print(clean_output)
+    if 'stdout' in clean_output:
+        to_print = ' '.join(clean_output.split(' ')[1:]).strip()
+        if "ngspice" in to_print and "done" in to_print:
+            send_char_dat.put("Quitting ngspice")
+        elif "Note: 'quit' asks for detaching ngspice.dll" in to_print:
+            pass
+        elif to_print not in string.whitespace:
+            send_char_dat.put(to_print)
+    elif 'stderr' in clean_output:
+        raise SystemError(" ".join(clean_output.split(' ')[1:]))
     return 0
 
 @CFUNCTYPE(c_int, c_char_p, c_int, c_void_p)
 def SendStat(sim_stat, lib_id, ret_ptr):
-    print(sim_stat)
-    print(lib_id)
+    send_stat_dat.put(sim_stat.decode())
     return 0
 
 
 # Initialize ngspice
-
+libngspice.ngSpice_Init(SendChar, SendStat, ControlledExit, None, None, None)
 
 # User functions
+def send_command(command):
+    # Clear the status queue for new commands
+    while not send_stat_dat.empty():
+        send_stat_dat.get_nowait()
+
+    libngspice.ngSpice_Command(create_string_buffer(command.encode()))
+    
+    output = []
+    
+    while not send_char_dat.empty():
+        output.append(send_char_dat.get_nowait())
+    return output
+
