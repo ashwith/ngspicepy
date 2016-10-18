497c442d8e5b1a0451215ccc139ebc2fb6192b20
Ashwith Jerome Rego <ashwith@gmail.com>
2016-10-19 01:10:13 +0530

    Added tests for get_data

--- test_ngspicepy.py
+++ test_ngspicepy.py
@@ -1,35 +1,58 @@
-from importlib.machinery import SourceFileLoader
-import pytest
-import mock
+import ngspicepy as ng
 
 import numpy as np
 
-ng = SourceFileLoader("ngspicepy", "../ngspicepy.py").load_module()
+import pytest
 
 
 class TestGetData:
 
-    def test_real():
+    def test_real(self):
         val = ng.get_data('e')
-        
+
         assert type(val) == np.ndarray
         assert len(val) == 1
-        
-        load_netlist('netlists/dc_ac_check.net')
-        
+        assert val.dtype == 'float64'
+        assert val[0] == pytest.approx(np.e)
 
+    def test_cmplx(self):
+        val = ng.get_data('i')
+        assert type(val) == np.ndarray
+        assert len(val) == 1
+        assert val.dtype == 'complex128'
+        assert val[0] == pytest.approx(1j)
 
-    def test_cmplx():
-        pass
+    def test_2args(self):
+        ng.load_netlist('./tests/netlists/dc_ac_check.net')
+        ng.run_dc('v1 0 1 0.1')
+        ng.run_dc('v1 0 1 0.2')
 
-    def test_2args():
-        pass
+        val = ng.get_data('v-sweep', 'dc1')
+        assert val.dtype == 'float64'
+        assert len(val) == 11
 
-    def test_1arg_no_plot():
-        pass
+    def test_1arg_no_plot(self):
+        ng.load_netlist('./tests/netlists/dc_ac_check.net')
+        ng.run_dc('v1 0 1 0.1')
+        ng.run_dc('v1 0 1 0.2')
 
-    def test_1arg_plot():
-        pass
+        val = ng.get_data('v-sweep')
+        assert val.dtype == 'float64'
+        assert len(val) == 6
 
-    def test_invalid_vector_name():
-        pass
+    def test_1arg_plot(self):
+        ng.load_netlist('./tests/netlists/dc_ac_check.net')
+        ng.run_dc('v1 0 1 0.1')
+        ng.run_dc('v1 0 1 0.2')
+
+        val = ng.get_data('dc1.v-sweep')
+        assert val.dtype == 'float64'
+        assert len(val) == 11
+
+    def test_invalid_vector_name(self):
+        ng.load_netlist('./tests/netlists/dc_ac_check.net')
+        ng.run_dc('v1 0 1 0.1')
+        ng.run_dc('v1 0 1 0.2')
+
+        with pytest.raises(ValueError):
+            ng.get_data('dc1.v-swoop')
