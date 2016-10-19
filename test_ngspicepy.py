d2d6c8728a7833d1d5bb0f344406023bae0c8c56
Jyoti Dhakal <jyoti.dhakal09@gmail.com>
2016-10-19 16:24:20 +0530

    Test Cases written for following functions:
    Functions: Send_C0mmand, run_dc, get_all_data, get_plot_names,
     get_vector_names

--- test_ngspicepy.py
+++ test_ngspicepy.py
@@ -5,6 +5,20 @@ import numpy as np
 import pytest
 
 
+class TestSendCommand:
+    def test_send_command():
+        val = send_command(' dc v1 0 1 .1 ')
+        assert isinstance(val,list)
+
+
+class TestRunDc:
+    def test_run_dc():
+        val=run_dc('v1 0 1 .1 v2 0 1 .1 ')
+        assert isinstance(val,list)   
+        
+
+
+
 class TestGetData:
 
     def test_real(self):
@@ -57,6 +71,15 @@ class TestGetData:
         with pytest.raises(ValueError):
             ng.get_data('dc1.v-swoop')
 
+class TestGetAllData:
+    def test_get_all_data():
+        ng.load_netlist('./tests/netlists/dc_ac_check.net')
+        ng.run_dc('v1 0 1 0.1')
+        val = get_all_data('dc1')
+        for i in val:
+            assert isinstance(i,np.array)
+
+class 
 
 class TestCurrentPlot:
 
@@ -65,6 +88,28 @@ class TestCurrentPlot:
         assert isinstance(val, str)
 
 
+class TestGetPlotNames:
+    def test_get_plot_names(self):
+        ng.load_netlist('./tests/netlists/dc_ac_check.net')
+        ng.run_dc('v1 0 1 0.1')
+        ng.run_dc('v1 0 1 0.2')
+        ng.run_dc('v1 0 1 0.3')
+        
+        val = get_plot_names()
+        assert isinstance(val,list)
+        assert val == ['dc1', 'dc2', 'dc3']
+        assert len(val) == 3
+        
+        
+class TestGetVectorNames:
+    def test_get_vector_names(self):
+        ng.load_netlist('./tests/netlists/dc_ac_check.net')
+        ng.run_dc('v1 0 1 0.1')
+        val = get_vector_names('dc1')
+        assert val == ['v1#branch','V(2)','V(1)','v-sweep']
+        
+
+                
 class TestLoadNetlist:
 
     def test_filename(self):
@@ -75,3 +120,4 @@ class TestLoadNetlist:
 
     def test_str(self):
         pass
+        
