2b9c34e4a6d6f26b814b635d9ecab2972b94d736
Ashwith Jerome Rego <ashwith@gmail.com>
2016-10-20 00:47:14 +0530

    Fixed tests, PEP8 fixes.

--- test_ngspicepy.py
+++ test_ngspicepy.py
@@ -18,24 +18,35 @@ ret_val.v_length = 10
 ret_val = pointer(ret_val)
 
 
+class TestGetPlotNames:
+    def test_get_plot_names(self):
+        ng.load_netlist('./tests/netlists/dc_ac_check.net')
+        ng.run_dc('v1 0 1 0.1')
+        ng.run_dc('v1 0 1 0.2')
+        ng.run_dc('v1 0 1 0.3')
+
+        val = ng.get_plot_names()
+        assert isinstance(val, list)
+        assert val == ['dc3', 'dc2', 'dc1', 'const']
+        assert len(val) == 4
+
+
 class TestSendCommand:
-    def test_send_command():
-        val = send_command(' dc v1 0 1 .1 ')
-        assert isinstance(val,list)
+    def test_send_command(self):
+        val = ng.send_command(' dc v1 0 1 .1 ')
+        assert isinstance(val, list)
 
 
 class TestRunDc:
-    def test_run_dc():
-        val=run_dc('v1 0 1 .1 v2 0 1 .1 ')
-        assert isinstance(val,list)   
-        
-
+    def test_run_dc(self):
+        val = ng.run_dc('v1 0 1 .1 v2 0 1 .1 ')
+        assert isinstance(val, list)
 
 
 class TestGetData:
 
     def test_real(self):
-        val = ng.get_data('e')
+        val = ng.get_data('const.e')
 
         assert type(val) == np.ndarray
         assert len(val) == 1
@@ -43,7 +54,7 @@ class TestGetData:
         assert val[0] == pytest.approx(np.e)
 
     def test_cmplx(self):
-        val = ng.get_data('i')
+        val = ng.get_data('const.i')
         assert type(val) == np.ndarray
         assert len(val) == 1
         assert val.dtype == 'complex128'
@@ -84,21 +95,21 @@ class TestGetData:
         with pytest.raises(ValueError):
             ng.get_data('dc1.v-swoop')
 
-class TestGetAllData:
-    def test_get_all_data():
-        ng.load_netlist('./tests/netlists/dc_ac_check.net')
-        ng.run_dc('v1 0 1 0.1')
-        val = get_all_data('dc1')
-        for i in val:
-            assert isinstance(i,np.array)
-
-class 
     @mock.patch('ngspicepy.libngspice.ngGet_Vec_Info',
                 return_value=ret_val)
     def test_api_call(self, mock_get_info):
         ng.get_data('v-sweep')
         # mock_get_info.assert_called_once_with(create_string_buffer(b'v-sweep'))
         assert mock_get_info.called
+
+
+class TestGetAllData:
+    def test_get_all_data(self):
+        ng.load_netlist('./tests/netlists/dc_ac_check.net')
+        ng.run_dc('v1 0 1 0.1')
+        val = ng.get_all_data('dc1')
+        for i in val:
+            assert type(i) == str
 
 
 class TestCurrentPlot:
@@ -114,28 +125,14 @@ class TestCurrentPlot:
         mock_CurPlot.assert_called_once_with()
 
 
-class TestGetPlotNames:
-    def test_get_plot_names(self):
-        ng.load_netlist('./tests/netlists/dc_ac_check.net')
-        ng.run_dc('v1 0 1 0.1')
-        ng.run_dc('v1 0 1 0.2')
-        ng.run_dc('v1 0 1 0.3')
-        
-        val = get_plot_names()
-        assert isinstance(val,list)
-        assert val == ['dc1', 'dc2', 'dc3']
-        assert len(val) == 3
-        
-        
 class TestGetVectorNames:
     def test_get_vector_names(self):
         ng.load_netlist('./tests/netlists/dc_ac_check.net')
         ng.run_dc('v1 0 1 0.1')
-        val = get_vector_names('dc1')
-        assert val == ['v1#branch','V(2)','V(1)','v-sweep']
-        
+        val = ng. get_vector_names('dc1')
+        assert val == ['v1#branch', 'V(2)', 'V(1)', 'v-sweep']
 
-                
+
 class TestLoadNetlist:
 
     def test_filename(self):
@@ -149,9 +146,6 @@ class TestLoadNetlist:
         assert isinstance(out, list)
 
     def test_str(self):
-
-        pass
-        
         with open('tests/netlists/dc_ac_check.net') as f:
             netlist = f.readlines()
         netlist = '\n'.join(netlist)
