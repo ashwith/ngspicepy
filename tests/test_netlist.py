1c5804d3d5a914db70298a8bb47990ca455b4499
Jyoti <jyoti.dhakal09@gmail.com>
2016-11-14 13:07:10 +0530

    Frame for test setup

--- tests/test_netlist.py
+++ tests/test_netlist.py
@@ -2,6 +2,52 @@ from ngspicepy import netlist
 
 import pytest
 
+
+class TestInit:
+    def test__init__(self):
+        pass
+
+
 class TestSetupSim:
     def test_setup_sim(self):
-        val = 
+        pass
+
+
+class TestRun:
+    def test_run(self):
+        pass
+
+
+class TestGetCurrentPlot:
+    def test_get_current_plot(self):
+        pass
+
+
+class TestGetPlots:
+    def test_get_plots(self):
+        pass
+
+
+class TestGetVectorNames:
+    def test_get_vector_names(self):
+        pass
+
+
+class TestGetVector:
+    def test_get_vector(self):
+        pass
+
+
+class TestGetVectors:
+    def test_get_vectors(self):
+        pass
+
+
+class TestCheckNetlist:
+    def test__checkNetlist__(self):
+        pass
+
+
+class TestStr:
+    def test__str__(self):
+        pass
