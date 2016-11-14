171de98c3e613933d0f752815db0c0065bcca16b
Jyoti <jyoti.dhakal09@gmail.com>
2016-11-14 15:45:15 +0530

    Test Cases Written
    For __init__, get_plots

--- tests/test_netlist.py
+++ tests/test_netlist.py
@@ -1,11 +1,41 @@
-from ngspicepy import netlist
+import sys
+
+import os
 
 import pytest
+
+import string
+
+import numpy
+
+module_path = os.path.dirname(os.path.curdir + os.path.sep)
+sys.path.insert(0, os.path.abspath(module_path))
+
+from ngspicepy import netlist as nt
+
+netlists_path = 'tests/netlists/'
 
 
 class TestInit:
     def test__init__(self):
-        pass
+        a = nt.Netlist(netlists_path + 'dc_ac_check.net')
+        assert isinstance(a, nt.Netlist)
+
+        with open(netlists_path + 'dc_ac_check.net') as f:
+            netlist = f.readlines()
+        a = nt.Netlist(netlists_path + 'dc_ac_check.net')
+        assert isinstance(a, nt.Netlist)
+
+        with open(netlists_path + 'dc_ac_check.net') as f:
+            netlist = f.readlines()
+        netlist = '\n'.join(netlist)
+        out = nt.Netlist(netlists_path + 'dc_ac_check.net')
+        assert isinstance(out, nt.Netlist)
+        with pytest.raises(ValueError):
+            nt.Netlist(netlists_path + ' ' + 'ac_dc_check.net')
+
+        with pytest.raises(TypeError):
+            nt.Netlist(123)
 
 
 class TestSetupSim:
@@ -20,27 +50,58 @@ class TestRun:
 
 class TestGetCurrentPlot:
     def test_get_current_plot(self):
-        pass
+        a = nt.Netlist(netlists_path + 'dc_ac_check.net')
+        a.setup_sim('dc', 'v1 0 1 .3')
+        a.run()
+        val = a.get_current_plot()
+        assert val == 'dc1'
+        assert isinstance(val, str)
 
 
 class TestGetPlots:
     def test_get_plots(self):
-        pass
+        a = nt.Netlist(netlists_path + 'dc_ac_check.net')
+        a.setup_sim('dc', 'v1 0 1 .3')
+        a.run()
+        a.setup_sim('dc', 'v1 0 1 .3')
+        a.run()
+        a.setup_sim('dc', 'v1 0 1 .3')
+        a.run()
+        val = a.get_plots()
+        assert len(val) == 5
+        assert val == ['dc4', 'dc3', 'dc2', 'dc1', 'const']
+        assert isinstance(val, list)
 
 
 class TestGetVectorNames:
     def test_get_vector_names(self):
-        pass
+        a = nt.Netlist(netlists_path + 'dc_ac_check.net')
+        a.setup_sim('dc', 'v1 0 1 .3')
+        a.run()
+        val = a.get_vector_names('dc1')
+        assert len(val) == 5
+        assert isinstance(val, list)
+        assert val == ['v1#branch', 'v2#branch', 'V(2)', 'V(1)', 'v-sweep']
 
 
 class TestGetVector:
     def test_get_vector(self):
-        pass
+        a = nt.Netlist(netlists_path + 'dc_ac_check.net')
+        a.setup_sim('dc', 'v1 0 1 .3')
+        a.run()
+        val = a.get_vector('V(1)', 'dc1')
+        assert isinstance(val, numpy.ndarray)
+        assert len(val) == 4
 
 
 class TestGetVectors:
     def test_get_vectors(self):
-        pass
+        a = nt.Netlist(netlists_path + 'dc_ac_check.net')
+        a.setup_sim('dc', 'v1 0 1 .3')
+        a.run()
+        val = a.get_vectors('dc1')
+        assert len(val) == 5
+        assert isinstance(val, dict)
 
 
 class TestCheckNetlist:
