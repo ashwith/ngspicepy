88ec42cd66e58fc9cbfc477ce09fa4345bf9cdcf
Ashwith Jerome Rego <ashwith@gmail.com>
2016-11-16 19:36:39 +0530

    Added package docstring for ngspicepy

--- ngspicepy/__init__.py
+++ ngspicepy/__init__.py
@@ -1,3 +1,21 @@
+"""
+ngspicepy
+=========
+
+ngspicepy as a python library for ngspice. It provides python wrappers for
+ngspice's C API along with other useful functions. This allows one to run SPICE
+simulations and get the data as numpy as arrays directly from python instead of
+having to use files to use the data in python. Python has better tools to
+process data and plot relavent results. Thus ngspicepy gives a bridge between
+ngspice's powerful simulator and python.
+
+
+The library can be used in two ways.
+
+1. Directly using the functions provided by ngspicepy.
+2. Using the Netlist class.
+
+"""
 from .ngspicepy import send_command, run_dc, run_ac, run_tran, run_op,\
 get_plot_names, current_plot, get_vector_names, get_data, get_all_data,\
 set_options, load_netlist, clear_plots, reset, libngspice
